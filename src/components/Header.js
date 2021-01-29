6114177086dda59c222ca48f9c4c327352076849
ccxex29 <blackccxex@gmail.com>
2021-01-29 21:35:26 +0700

    Fix styling especially for fonts
     - Add Red Hat Display font on header
    
     - Add Raleway on index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {IoLogoGithub} from 'react-icons/io5';
 import styles from '../assets/styles/Header.module.css';
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <span>Femmund</span>
+            <nav>
+                <a href={'https://github.com'}>
+                    <IoLogoGithub color={'#333'} />
+                </a>
+            </nav>
         </header>
     );
 };
