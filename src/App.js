7f9c3e824fef6916e2f6aa745cb3b97aa913889e
ccxex29 <blackccxex@gmail.com>
2021-01-29 19:23:20 +0700

    Add Initial styling to root elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
+import styles from './assets/styles/App.module.css'
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className={styles.root}>
     </div>
   );
 }
