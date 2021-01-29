8a73bca17d15808a71e7bb343f7851c50b0e1cc9
ccxex29 <blackccxex@gmail.com>
2021-01-30 01:37:22 +0700

    Add parcel package and Fix warning close button
     - Add parcel-bundler to package.json
    
     - Fix warning close button issue when resizing below the designated box
       size

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -1,12 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styles from '../assets/styles/Warning.module.css';
 import {IoCloseCircle} from 'react-icons/io5';
 import {FaHardHat} from 'react-icons/fa';
 
 const Warning = () => {
     const [showWarning, setShowWarning] = useState(true);
+    const [boxWidth, setBoxWidth] = useState(420);
+    const boxEl = useRef(null);
+    useEffect(() => {
+        window.addEventListener('resize', updateWidthState);
+        return (() => {
+            window.removeEventListener('resize', updateWidthState);
+        });
+    }, []);
     const removeWarning = () => {
         setShowWarning(() => false);
+    };
+    const updateWidthState = () => {
+        setBoxWidth(() => boxEl.current.offsetWidth);
     };
 
     return (
@@ -14,13 +25,13 @@ const Warning = () => {
         {
             showWarning ?
             <div className={styles.warningWrapper}>
-                <div className={styles.warningBox}>
+                <div className={styles.warningBox} ref={boxEl}>
                         <span>
                             <FaHardHat size={16} style={{position: 'relative', marginRight: 16, top: 1}} />
                             This site is under development
                         </span>
-                    <div className={styles.closeBtn} onClick={removeWarning}>
-                        <div className={styles.closeBtnFill}></div>
+                    <div className={styles.closeBtn} onClick={removeWarning} style={{ marginLeft: boxWidth - (32/2) }}>
+                        <div className={styles.closeBtnFill}/>
                         <IoCloseCircle size={32} color={'#666'}/>
                     </div>
                 </div>
